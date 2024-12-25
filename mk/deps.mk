a03fd2e2dd9a209d0bfa72f6b0bb9c3f590ea82e
orbea <orbea@riseup.net>
2024-12-25 12:01:10 -0800

    mk: don't hardcode pkgconfig names

--- mk/deps.mk
+++ mk/deps.mk
@@ -6,20 +6,26 @@
 
 CFLAGS_JG = $(shell $(PKG_CONFIG) --cflags jg)
 
-CFLAGS_EPOXY = $(shell $(PKG_CONFIG) --cflags epoxy)
-LIBS_EPOXY = $(shell $(PKG_CONFIG) --libs epoxy)
+PKGCONF_EPOXY = epoxy
+CFLAGS_EPOXY = $(shell $(PKG_CONFIG) --cflags $(PKGCONF_EPOXY))
+LIBS_EPOXY = $(shell $(PKG_CONFIG) --libs $(PKGCONF_EPOXY))
 
-CFLAGS_FLAC = $(shell $(PKG_CONFIG) --cflags flac)
-LIBS_FLAC = $(shell $(PKG_CONFIG) --libs flac)
+PKGCONF_FLAC = flac
+CFLAGS_FLAC = $(shell $(PKG_CONFIG) --cflags $(PKGCONF_FLAC))
+LIBS_FLAC = $(shell $(PKG_CONFIG) --libs $(PKGCONF_FLAC))
 
-CFLAGS_LZO = $(shell $(PKG_CONFIG) --cflags lzo2)
-LIBS_LZO = $(shell $(PKG_CONFIG) --libs lzo2)
+PKGCONF_LZO = lzo2
+CFLAGS_LZO = $(shell $(PKG_CONFIG) --cflags $(PKGCONF_LZO))
+LIBS_LZO = $(shell $(PKG_CONFIG) --libs $(PKGCONF_LZO))
 
-CFLAGS_SDL2 = $(shell $(PKG_CONFIG) --cflags sdl2)
-LIBS_SDL2 = $(shell $(PKG_CONFIG) --libs sdl2)
+PKGCONF_SDL2 = sdl2
+CFLAGS_SDL2 = $(shell $(PKG_CONFIG) --cflags $(PKGCONF_SDL2))
+LIBS_SDL2 = $(shell $(PKG_CONFIG) --libs $(PKGCONF_SDL2))
 
-CFLAGS_ZLIB = $(shell $(PKG_CONFIG) --cflags zlib)
-LIBS_ZLIB = $(shell $(PKG_CONFIG) --libs zlib)
+PKGCONF_ZLIB = zlib
+CFLAGS_ZLIB = $(shell $(PKG_CONFIG) --cflags $(PKGCONF_ZLIB))
+LIBS_ZLIB = $(shell $(PKG_CONFIG) --libs $(PKGCONF_ZLIB))
 
-CFLAGS_ZSTD = $(shell $(PKG_CONFIG) --cflags libzstd)
-LIBS_ZSTD = $(shell $(PKG_CONFIG) --libs libzstd)
+PKGCONF_ZSTD = libzstd
+CFLAGS_ZSTD = $(shell $(PKG_CONFIG) --cflags $(PKGCONF_ZSTD))
+LIBS_ZSTD = $(shell $(PKG_CONFIG) --libs $(PKGCONF_ZSTD))
