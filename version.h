b2be5a8df827dae1f0bf656c1070b9f6b6bafcbf
Rupert Carmichael <5050061-carmiker@users.noreply.gitlab.com>
2026-07-02 11:23:15 -0400

    Version 2.1.1

--- version.h
+++ version.h
@@ -5,9 +5,9 @@
 # https://semver.org/
 VERSION_MAJOR=2
 VERSION_MINOR=1
-VERSION_PATCH=0
+VERSION_PATCH=1
 #endif
 
 #ifndef JG_VERSION
-#define JG_VERSION "2.1.0"
+#define JG_VERSION "2.1.1"
 #endif
